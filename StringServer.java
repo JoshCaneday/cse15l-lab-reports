5ad77c7d7f1e9a055a7db4c519840d468c542120
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2023-10-18 16:41:56 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -2,11 +2,8 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     int count = 0;
     String previous = "";
-
     public String handleRequest(URI url) {
         if (url.getPath().equals("/add-message")) {
             String[] parameters = url.getQuery().split("=");
@@ -16,22 +13,17 @@ class Handler implements URLHandler {
                 previous = previous.replace("+"," ");
                 return String.format("%s\n", previous);
             }
-            
         } 
             return "Use /add-message keyword!";
         }
     }
-
-
 class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            System.out.println("Ports can only be from 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         myServer.start(port, new Handler());
     }
 }
